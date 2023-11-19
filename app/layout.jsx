71a77ca332664a07eb7046ce7ffddb576e9e1edc
ram5353 <ramkiran5353@gmail.com>
2023-11-18 21:59:41 -0700

    refactored changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  title: "Memories in words",
+  description: "Discover & Share",
 };
 
 const RootLayout = ({ children }) => (
--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,10 @@ const Home = () => (
     <h1 className='head_text text-center'>
       Discover & Share
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+      <span className='orange_gradient text-center'> Share your Opinion</span>
     </h1>
     <p className='desc text-center'>
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+      Creating memories, capturing them in words, and saving them as a written keepsake — a timeless collection to cherish and revisit.
     </p>
 
     <Feed />
